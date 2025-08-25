8aa806822e6cf3f7094fd1e5236c3cda10890f8d
Bru Mas Ribera <brumasribera@gmail.com>
2025-08-25 10:11:11 +0200

    Fix WegawPage filename case sensitivity for consistent naming

--- dist/assets/external-link-DLxQSfTS.js
+++ dist/assets/external-link-DLxQSfTS.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-DXydvrGq.js";/**
+import{c as a}from"./index-BZgE8fYh.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- dist/assets/trending-up-C0RE1X2r.js
+++ dist/assets/trending-up-C0RE1X2r.js
@@ -1,4 +1,4 @@
-import{c as n}from"./index-DXydvrGq.js";/**
+import{c as n}from"./index-BZgE8fYh.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.

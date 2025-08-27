e3fcee1ca519867dade6f2e82a86b912287eccd3
Bru Mas Ribera <brumasribera@gmail.com>
2025-08-27 14:10:38 +0200

    v1.1.8: Fix VERSION.json not being deployed by copying to public folder

--- public/sw.js
+++ public/sw.js
@@ -11,7 +11,7 @@ const urlsToCache = [
   '/tools/timer/manifest.webmanifest'
 ];
 
-// Timer Service Worker - Version 1.1.7 - Released 2025-08-27 12:08:17// Timer functionality for background operation
+// Timer Service Worker - Version 1.1.8 - Released 2025-08-27 12:10:23// Timer functionality for background operation
 let timerStartTime = null;
 let timerDuration = 8 * 60; // 8 minutes in seconds
 let animationFrameId = null;

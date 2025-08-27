a7243e855fd07082f78733cee18f8d9cd551efcf
Bru Mas Ribera <brumasribera@gmail.com>
2025-08-27 19:54:22 +0200

    v1.1.16: Fix Android audio to preserve background music during timer gongs

--- dist/sw.js
+++ dist/sw.js
@@ -11,7 +11,7 @@ const urlsToCache = [
   '/tools/timer/manifest.webmanifest'
 ];
 
-// Timer Service Worker - Version 1.1.15 - Released 2025-08-27 17:52:28// Timer functionality for background operation
+// Timer Service Worker - Version 1.1.16 - Released 2025-08-27 17:54:08// Timer functionality for background operation
 let timerStartTime = null;
 let timerDuration = 8 * 60; // 8 minutes in seconds
 let animationFrameId = null;
